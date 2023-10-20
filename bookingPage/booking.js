86e7f1ff0b08bc5300058fe3562639d5dcc5666e
Huyen Ho <huyen.hothi25@student.passerellesnumeriques.org>
2023-10-20 15:41:30 +0700

    add push infomation

--- bookingPage/booking.js
+++ bookingPage/booking.js
@@ -1,62 +1,58 @@
-const button = document.querySelector('.button-review');
-
-async function Booking() {
-  var countAdults    = document.getElementById('adults-input').value;
-  var countChildrens = document.getElementById('children-input').value;
-  var startDate      = document.getElementById('start-date-input').value;
-  var duration       = document.getElementById('duration-input').value;
-  var totalPrice     = document.getElementById('totalPrice').value;
-
-  var fullName       = document.getElementById('fullname-input').value;
-  var gmail          = document.getElementById('email-input').value;
-  var telephone      = document.getElementById('tel-input').value;
-  var request        = document.getElementById('request-input').value;
-
-  await axios.post("https://touring.glitch.me/booking", {
-   
-            role: "user",
-            status: "Active",
-            name: fullName,
-            adults: countAdults,
-            children: countChildrens,
-            start_date: startDate,
-            duration: duration,
-
-            total_price: totalPrice,
-            email: gmail,
-            phone: telephone,
-            additional_request: request,
-  });
-    
+function getDestinationIdFromURL() {
+  const urlParams = new URLSearchParams(window.location.search);
+  return parseInt(urlParams.get('id'));
 }
 
-function booking_review() {
-  
-    let overlay = document.getElementById('complete');
-    let backgroundColor_nav1 = document.getElementById('circle-2');
-    let backgroundColor_nav2 = document.getElementById('circle-3');
-    setTimeout(() => {
-      backgroundColor_nav2.style.backgroundColor = 'var(--button-3)';
-      backgroundColor_nav2.style.color = 'var(--main-white)';
-      backgroundColor_nav1.style.backgroundColor = 'var(--button-4)';
-      backgroundColor_nav1.style.color = 'var(--main-text)';
-      overlay.style.display = 'flex';
-    }, 1000);
-  ;
+handleGetInfo(getDestinationIdFromURL());
+let id_des = getDestinationIdFromURL();
+let id_u = 1;
+let childPrice;
+let adultPrice;
+async function handleGetInfo(id) {
+  await axios
+  .get(`https://touring.glitch.me/details/${id}`)
+    .then((response) => {
+      document.getElementById("info-tour-title").innerHTML = response.data.name_destination;
+      document.getElementById("review-tour-title").innerHTML = response.data.name_destination;
+      childPrice = response.data.childPrice,
+      adultPrice = response.data.adultPrice
+    })
+    .catch(error => {
+      // Xử lý lỗi khi yêu cầu thất bại
+      console.log("Error");
+    });
 }
-
-button.addEventListener('click', Booking);
-button.addEventListener('click', booking_review);
-
-
-
+var adults;
+var start;
+var children;
+var duration;
+var fullname;
+var phone;
+var email;
+var request;
+let price;
 
 function checkout_review() {
-  let review                = document.getElementById('review-form');
-  let booking               = document.getElementById('booking-form');
-  let backgroundColor_nav1  = document.getElementById('circle-1');
-  let backgroundColor_nav2  = document.getElementById('circle-2');
-  let color_nav1            = document.getElementById('so1');
+  // lấy giá trị từ ô input
+  adults = document.querySelector('input[id="adults-input"]').value;
+  console.log(adults);
+  start = document.querySelector('input[id="start-date-input"]').value;
+  children = document.querySelector('input[id="children-input"]').value;
+  console.log(children);
+  duration = document.querySelector('input[id="duration-input"]').value;
+  
+  fullname = document.querySelector('input[id="fullname-input"]').value;
+  phone = document.querySelector('input[id="tel-input"]').value;
+  email = document.querySelector('input[id="email-input"]').value;
+  request = document.querySelector('textarea#request-input').value;
+  //tạo giá tiền
+  price = (adultPrice*adults)+(childPrice*children);
+
+  let review = document.getElementById('review-form');
+  let booking = document.getElementById('booking-form');
+  let backgroundColor_nav1 = document.getElementById('circle-1');
+  let backgroundColor_nav2 = document.getElementById('circle-2');
+  let color_nav1 = document.getElementById('so1');
 
   setTimeout(() => {
     review.style.display = 'flex';
@@ -66,90 +62,42 @@ function checkout_review() {
     backgroundColor_nav1.style.backgroundColor = 'var(--button-4)';
     color_nav1.style.color = 'var(--main-text)';
   }, 1000);
+  handleGetDetail();
 }
 
 
-document.getElementById('complete').addEventListener('click', function() {
-  this.style.display = 'none';
-});
 
+//SHOW INFOMATION
+async function handleGetDetail() {
+  let id = id_des;
+  document.querySelector('input[id="review-adults-input"]').value = adults;
+  document.querySelector('input[id="review-start-date-input"]').value = start;
+  document.querySelector('input[id="review-children-input"]').value = children;
+  document.querySelector('input[id="review-duration-input"]').value = duration;
+  document.querySelector('textarea#review-customer-info').value = fullname + "\n" + phone + "\n" + email;
+  document.querySelector('textarea#review-customer-request').value = request;
+  document.querySelector('input[id="review-price"]').value = price;
+}
 
-
-document.addEventListener("DOMContentLoaded", function () {
-  const adultsInput = document.getElementById("adults-input");
-  const childrenInput = document.getElementById("children-input");
-  const totalPriceElement = document.getElementById("totalPrice");
-
-  function calculateTotal() {
-      const adults = parseInt(adultsInput.value) || 0;
-      const children = parseInt(childrenInput.value) || 0;
-      const destinationId = getDestinationIdFromURL(); 
-    
-      fetch("../Database/db.json")
-          .then(response => response.json())
-          .then(data => {
-              const details = data.details_destination;
-              const detail = details.find(item => item.id_destination === destinationId);
-
-              if (detail) {
-                  const adultPrice = parseInt(detail.adultPrice);
-                  const childPrice = parseInt(detail.childPrice);
-                  const total = adults * adultPrice + children * childPrice;
-                  totalPriceElement.value = total;
-              }
-          })
-          .catch(error => console.error("Lỗi khi tải dữ liệu: " + error));
-  }
-
-
-  function getDestinationIdFromURL() {
-      const urlParams = new URLSearchParams(window.location.search);
-      return parseInt(urlParams.get('id'));
-  }
-
-  adultsInput.addEventListener("input", calculateTotal);
-  childrenInput.addEventListener("input", calculateTotal);
-
-  calculateTotal();
-});
-
-
-
-
-//sự kiện bắt click vào button CHECKOUT
-// document.getElementById('button-review').addEventListener('click', function(e) {
-//   let overlay = document.getElementById('complete');
-//   let backgroundColor_nav1 = document.getElementById('circle-2');
-//   let backgroundColor_nav2 = document.getElementById('circle-3');
-//   setTimeout(() => {
-//     backgroundColor_nav2.style.backgroundColor = 'var(--button-3)';
-//     backgroundColor_nav2.style.color = 'var(--main-white)';
-//     backgroundColor_nav1.style.backgroundColor = 'var(--button-4)';
-//     backgroundColor_nav1.style.color = 'var(--main-text)';
-//     overlay.style.display = 'flex';
-//   }, 1000);
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// POST INFOMATION 
+async function handleSubmit(event) {
+  event.preventDefault();
+  await axios
+    .post("https://touring.glitch.me/bookings", {
+      id_destination: id_des,
+      id_user: id_u,
+      booking_fullname: fullname,
+      booking_phone_number: phone,
+      booking_email: email,
+      total_adults: adults,
+      total_children: children,
+      total_price: price,
+      start_date: start,
+      duration: duration,
+      request: request
+    })
+    .then((response) => {
+      var modal = document.getElementById("complete");
+      modal.style.display = "flex";
+    });
+}
