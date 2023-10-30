483d15350b9038196ae9f8028829b28d5307bbcc
Huyen Ho <huyen.hothi25@student.passerellesnumeriques.org>
2023-10-30 17:11:40 +0700

    huyen change 30/10

--- HeaderFooter/header.js
+++ HeaderFooter/header.js
@@ -14,7 +14,6 @@
 //     // signUpModal.style.display = "block";
 // }
 
-
 // // Hàm mở modal Sign Up
 // function openSignUpModal() {
 //     var loginModal = document.getElementById("loginModal");
@@ -29,10 +28,6 @@
 //     signUpModal.style.display = "none";
 //     // loginModal.style.display = "block";
 // }
-
-
-
-
 
 // async function handleSubmit() {
 
@@ -64,8 +59,6 @@
 
 // }
 
-
-
 // // async function handleSubmit() {
 // //     var email = document.getElementById('emailLogIn').value;
 // //     var password = document.getElementById('passwordLogIn').value;
@@ -83,9 +76,6 @@
 // //             }
 // //         });
 // // }
-
-
-
 
 // function logout() {
 
@@ -110,12 +100,10 @@
 //     }
 // });
 
-
 // async function handleSignup() {
 //     var email = document.getElementById('email').value;
 //     var phone = document.getElementById('phone').value;
 //     var password = document.getElementById('password').value;
-
 
 //     await axios
 //         .post("https://touring.glitch.me/users", {
@@ -158,11 +146,6 @@
 //     localStorage.setItem("isLogin", false);
 // }
 
-
-
-    
-
-
 var isLogin = JSON.parse(localStorage.getItem("isLogin"));
 
 // Hàm mở modal Loginá
@@ -173,14 +156,12 @@ function openLoginModal() {
     signUpModal.style.display = "none";
 }
 
-
 // Hàm đóng modal Login
 function closeLoginModal() {
     var loginModal = document.getElementById("loginModal");
     loginModal.style.display = "none";
     // signUpModal.style.display = "block";
 }
-
 
 // Hàm mở modal Sign Up
 function openSignUpModal() {
@@ -197,43 +178,35 @@ function closeSignUpModal() {
     // loginModal.style.display = "block";
 }
 
-
-
 async function handleLogin() {
+    var email = document.getElementById("emailLogIn").value;
+    var password = document.getElementById("passwordLogIn").value;
 
-    var email = document.getElementById('emailLogIn').value;
-    var password = document.getElementById('passwordLogIn').value;
+    await axios.get("https://touring.glitch.me/users").then((response) => {
+        var userExist = response.data.find((usr) => usr.email === email);
+        if (userExist && userExist.password === password) {
+            localStorage.setItem("isLogin", true);
+            setTimeout(() => {
+                location.href = `${location.origin}/HomePage/homepage.html`;
+            }, 100);
+        }
+    });
 
-    await axios
-        .get("https://touring.glitch.me/users")
-        .then((response) => {
+    localStorage.setItem("isLogin", "true");
 
-            var userExist = response.data.find((usr) => usr.email === email);
-            if (userExist && userExist.password === password) {
-                localStorage.setItem("isLogin", true);
-                setTimeout(() => {
-                    location.href = `${location.origin}/Final_Basic_Web_Design/HomePage/homepage.html`;
-                }, 100);
-            }
-        });
-
-    localStorage.setItem("isLoggedIn", "true");
-
-    document.getElementById("userPic").src = "./user1.png"
+    document.getElementById("userPic").src = "./user1.png";
     document.getElementById("userInfoPic").src = "./user1.png";
     document.getElementById("loggedInUserName").textContent = "Thanh Luan";
     document.getElementById("subMenuLogin").style.display = "none";
     document.getElementById("subMenuUser").style.display = "block";
-
 }
-
 
 // Hàm logout khi người dùng đăng xuất
 function logoutUser() {
     localStorage.setItem("isLogin", false);
 
     document.getElementById("userInfoPic").src = "./user2.png";
-    document.getElementById("userPic").src = "./user2.png"
+    document.getElementById("userPic").src = "./user2.png";
     document.getElementById("loggedInUserName").textContent = "Guest";
     document.getElementById("subMenuLogin").style.display = "block";
     document.getElementById("subMenuUser").style.display = "none";
@@ -244,66 +217,94 @@ document.addEventListener("DOMContentLoaded", function () {
     var isLoggedIn = localStorage.getItem("isLoggedIn");
     if (isLoggedIn === "true") {
         document.getElementById("userInfoPic").src = "./user1.png";
-        document.getElementById("userPic").src = "./user1.png"
+        document.getElementById("userPic").src = "./user1.png";
         document.getElementById("loggedInUserName").textContent = "Thanh Luan";
         document.getElementById("subMenuLogin").style.display = "none";
         document.getElementById("subMenuUser").style.display = "block";
     }
 });
 
-
-
 async function handleSignup() {
-    var email = document.getElementById('email').value;
-    var phone = document.getElementById('phone').value;
-    var password = document.getElementById('password').value;
+    var email = document.getElementById("email").value;
+    // var phone = document.getElementById('phone').value;
+    var password = document.getElementById("password").value;
 
-
-    await axios
-        .post("https://touring.glitch.me/users", {
+    try {
+        const response = await axios.post("https://touring.glitch.me/users", {
             email: email,
             password: password,
-            phone: phone,
-            role: "",
+            role: "Customer",
             name: "",
+            avatar: "",
             status: "Active",
-        })
+        });
 
-        .then((response) => {
-            toastr.success("Signup successfully", "Message", {
-                timeOut: 2000,
-                closeButton: true,
-                debug: false,
-                newestOnTop: true,
-                progressBar: true,
-                positionClass: "toast-top-right",
-                preventDuplicates: true,
-                onclick: null,
-                showDuration: "300",
-                hideDuration: "1000",
-                extendedTimeOut: "1000",
-                showEasing: "swing",
-                hideEasing: "linear",
-                showMethod: "fadeIn",
-                hideMethod: "fadeOut",
-                tapToDismiss: false,
-            });
-            setTimeout(() => {
-                location.href = `${location.origin}header.html`;
-            }, 200);
+        // Lấy ID của người dùng từ phản hồi
+        const userId = response.data.id;
+
+        // Tiếp tục xử lý với ID của người dùng
+        console.log("ID của người dùng:", userId);
+        localStorage.setItem("userId", userId);
+
+        //gọi function 
+        sendEmail(email);
+    } catch (error) {
+        // Xử lý lỗi nếu có
+        console.error("Lỗi khi tạo người dùng:", error);
+    }
+}
+
+function sendEmail(email) {
+    emailjs.init('Osy8L38k62YRgUKem'); // Thay 'YOUR_USER_ID' bằng User ID của bạn
+
+    const serviceID = "service_0ohuk3r"; // Thay 'YOUR_SERVICE_ID' bằng Service ID của dịch vụ của bạn
+    const templateID = "template_pgj7ajo"; // Thay 'YOUR_TEMPLATE_ID' bằng ID của mẫu email của bạn
+
+    var templateParams = {
+        name: "Customer of Travel Agency",
+        email: email
+    };
+
+    emailjs.send(serviceID, templateID, templateParams)
+        .then(function (response) {
+            console.log("Email sent successfully!", response.status, response.text);
+        })
+        .catch(function (error) {
+            console.error("Email failed to send:", error);
         });
 }
 
-handleSignup;
-handleSignup();
 
+
+// .then((response) => {
+
+//     toastr.success("Signup successfully", "Message", {
+//         timeOut: 2000,
+//         closeButton: true,
+//         debug: false,
+//         newestOnTop: true,
+//         progressBar: true,
+//         positionClass: "toast-top-right",
+//         preventDuplicates: true,
+//         onclick: null,
+//         showDuration: "300",
+//         hideDuration: "1000",
+//         extendedTimeOut: "1000",
+//         showEasing: "swing",
+//         hideEasing: "linear",
+//         showMethod: "fadeIn",
+//         hideMethod: "fadeOut",
+//         tapToDismiss: false,
+//     });
+//     setTimeout(() => {
+//         location.href = `${location.origin}header.html`;
+//     }, 200);
+// });
+// }
+
+// handleSignup;
+// handleSignup();
 
 async function logout() {
-        localStorage.setItem("isLogin", false);
-    }
-
-
-
-
-
-
+    localStorage.setItem("isLogin", false);
+}
--- HeaderFooter/loadHeaderFooter.js
+++ HeaderFooter/loadHeaderFooter.js
@@ -1,13 +1,13 @@
 
 function loadHeaderFooter() {
-    fetch('/Final_Basic_Web_Design/HeaderFooter/header.html')
+    fetch('/HeaderFooter/header.html')
         .then(response => response.text())
         .then(data => {
             const header = document.getElementById('header');
             header.innerHTML = data;
         });
 
-    fetch('/Final_Basic_Web_Design/HeaderFooter/footer.html')
+    fetch('/HeaderFooter/footer.html')
         .then(response => response.text())
         .then(data => {
             const footer = document.getElementById('footer');
